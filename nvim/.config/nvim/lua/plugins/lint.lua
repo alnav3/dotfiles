a63145de2ee3702f032d6bb03df4bbce88729c7f
Alex Navia <developer@alnav.dev>
2024-06-28 12:38:26 +0000

    modify route to use standar. This is the first one i saw, i'll keep updating in the future

--- nvim/.config/nvim/lua/plugins/lint.lua
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -7,7 +7,8 @@ return {
             "williamboman/mason.nvim"
         },
         config = function()
-            local sonar_language_server_path = "/Users/alexnavia3/.dotfiles/data/nvim/mason/packages/sonarlint-language-server"
+            local nvim_data_path = vim.fn.stdpath("data")
+            local sonar_language_server_path = nvim_data_path .. "/mason/packages/sonarlint-language-server"
             local analyzers_path = sonar_language_server_path .. "/extension/analyzers"
             require("sonarlint").setup({
                 server = {
@@ -18,7 +19,6 @@ return {
                         vim.fn.expand(analyzers_path .. "/sonarpython.jar"),
                         vim.fn.expand(analyzers_path .. "/sonargo.jar"),
                         vim.fn.expand(analyzers_path .. "/sonarjava.jar"),
-
                     }
                 },
                 filetypes = {
