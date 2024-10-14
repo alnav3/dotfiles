dba58c58da01b35285dfebf75e3e9601c68cd004
Alex Navia <developer@alnav.dev>
2024-10-14 19:47:57 +0200

    forgot to return it

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,4 +1,4 @@
-{
+return {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
     opts = {},
