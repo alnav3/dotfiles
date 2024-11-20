a9e3e3e2b83efc03224b34bb28da47aa3de6cdfc
Alex Navia <developer@alnav.dev>
2024-11-20 09:23:05 +0100

    testing

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -58,26 +58,7 @@ return {
     {
         "leoluz/nvim-dap-go",
         config = function()
-            require("dap-go").setup() {
-                delve = {
-                    path = "dlv",
-                    initialize_timeout_sec = 20,
-                    args = {
-                        "--", "CGO_CFLAGS=-O2", -- Pass CGO_CFLAGS as an argument
-                    },
-                    build_flags = {},
-                    detached = vim.fn.has("win32") == 0,
-                    cwd = nil,
-                    -- Alternatively, use an environment variable setup if delve supports it
-                    env = {
-                        CGO_CFLAGS = "-O2",
-                    },
-                },
-                tests = {
-                    verbose = false,
-                },
-            }
-        end,
-    },
+            require("dap-go").setup()
+        },
 }
 
