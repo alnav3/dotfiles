85f8c38808e56002de6cfa0a5f185c0bcdab5473
Alex Navia <developer@alnav.dev>
2024-12-10 14:35:01 +0100

    changed branch and debug

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -22,13 +22,11 @@ return {
     },
     {
         "CopilotC-Nvim/CopilotChat.nvim",
-        branch = "canary",
         dependencies = {
             { "github/copilot.vim" }, -- or github/copilot.vim
             { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
         },
         opts = {
-            debug = true, -- Enable debugging
             window = {
                 layout = 'float',
                 width = 1,
